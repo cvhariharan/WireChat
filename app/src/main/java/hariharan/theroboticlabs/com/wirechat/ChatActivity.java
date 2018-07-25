54845fbf98198664863a475b92a430337bc09d35
C.V.Hariharan <cvhariharan@protonmail.com>
2018-07-25 17:02:09 +0530

    Implemented chat viewmodel.

--- app/src/main/java/hariharan/theroboticlabs/com/wirechat/ChatActivity.java
+++ app/src/main/java/hariharan/theroboticlabs/com/wirechat/ChatActivity.java
@@ -1,17 +1,30 @@
 package hariharan.theroboticlabs.com.wirechat;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.List;
+import java.util.zip.Inflater;
+
+import hariharan.theroboticlabs.com.wirechat.Utils.ChatMessage;
 import hariharan.theroboticlabs.com.wirechat.Utils.FirebaseUtils;
+import hariharan.theroboticlabs.com.wirechat.Viewmodels.ChatViewModel;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -19,6 +32,8 @@ public class ChatActivity extends AppCompatActivity {
     private EditText messageField;
     private RecyclerView chatRecycler;
     private String toUid, toName;
+    private ChatViewModel chatViewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +48,9 @@ public class ChatActivity extends AppCompatActivity {
 
         messageField = (EditText) findViewById(R.id.message_window);
         chatRecycler = (RecyclerView) findViewById(R.id.chat_recycler);
+        chatRecycler.setHasFixedSize(true);
+        chatRecycler.setLayoutManager(new LinearLayoutManager(this));
+
         Button sendButton = (Button) findViewById(R.id.send_button);
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,10 +60,63 @@ public class ChatActivity extends AppCompatActivity {
                     sendMessage(message);
             }
         });
+
+        chatViewModel = ViewModelProviders.of(this).get(ChatViewModel.class);
+        chatViewModel.setChatPair(FirebaseAuth.getInstance().getCurrentUser().getUid(),
+                toUid);
+        chatViewModel.getMessages().observe(this, new Observer<List<ChatMessage>>() {
+            @Override
+            public void onChanged(@Nullable List<ChatMessage> chatMessages) {
+                ChatRecycler adapter = new ChatRecycler(chatMessages);
+                chatRecycler.setAdapter(adapter);
+            }
+        });
+
     }
 
     private void sendMessage(String message) {
         firebaseUtils.sendMessage(FirebaseAuth.getInstance().getCurrentUser().getUid(),
                 toUid, message);
+    }
+
+    class ChatRecycler extends RecyclerView.Adapter<ChatRecycler.ChatViewHolder>{
+
+        private List<ChatMessage> allMessages;
+        public ChatRecycler(List<ChatMessage> messages) {
+            this.allMessages = messages;
+        }
+
+        @Override
+        public ChatViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View mView = LayoutInflater.from(ChatActivity.this).inflate(R.layout.chat_bubble, parent, false);
+            ChatViewHolder viewHolder = new ChatViewHolder(mView);
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(ChatViewHolder holder, int position) {
+            holder.message = allMessages.get(position).getMessage();
+            holder.from = allMessages.get(position).getFrom();
+            holder.to = allMessages.get(position).getTo();
+            holder.messageView.setText(holder.message);
+            if(holder.from.equals(FirebaseAuth.getInstance().getCurrentUser().getUid())) {
+                //Left align messages received and right align messages sent
+                holder.messageView.setGravity(Gravity.RIGHT);
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return allMessages.size();
+        }
+
+        class ChatViewHolder extends RecyclerView.ViewHolder {
+            public String from, to, message;
+            public TextView messageView;
+            public ChatViewHolder(View itemView) {
+                super(itemView);
+                messageView = itemView.findViewById(R.id.chat);
+            }
+        }
     }
 }
--- app/src/main/java/hariharan/theroboticlabs/com/wirechat/Viewmodels/ChatViewModel.java
+++ app/src/main/java/hariharan/theroboticlabs/com/wirechat/Viewmodels/ChatViewModel.java
@@ -1,8 +1,77 @@
 package hariharan.theroboticlabs.com.wirechat.Viewmodels;
 
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import hariharan.theroboticlabs.com.wirechat.Utils.ChatMessage;
+import hariharan.theroboticlabs.com.wirechat.Utils.FirebaseUtils;
+
 /**
  * Created by hariharan on 7/25/18.
  */
 
-public class ChatViewModel {
+public class ChatViewModel extends ViewModel{
+
+    private DatabaseReference databaseRef;
+    private FirebaseUtils firebaseUtils;
+    private String uid, fuid;
+    private List<ChatMessage> allMessages;
+    private MutableLiveData<List<ChatMessage>> messagesLiveData;
+
+    public ChatViewModel() {
+        allMessages = new ArrayList<>();
+        messagesLiveData = new MutableLiveData<>();
+        firebaseUtils = new FirebaseUtils();
+        databaseRef = firebaseUtils.getDatabaseRef();
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public String getFuid() {
+        return fuid;
+    }
+
+    public void setChatPair(String uid, String fuid) {
+        this.uid = uid;
+        this.fuid = fuid;
+        databaseRef.child(this.uid).child(this.fuid)
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if(!allMessages.isEmpty())
+                            allMessages.clear();
+
+                        for(DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                            if(snapshot.hasChild("from")) {
+                                ChatMessage message = snapshot.getValue(ChatMessage.class);
+                                add(message);
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+    }
+
+    private void add(ChatMessage message) {
+        allMessages.add(message);
+        messagesLiveData.postValue(allMessages);
+    }
+
+    public MutableLiveData<List<ChatMessage>> getMessages() {
+        return messagesLiveData;
+    }
 }
